f037dd6780554b37755fb96a05a5ddce35b0c251
Poojan <poojangoyani@gmail.com>
2025-08-27 09:41:58 +0530

    using port from dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-
+const PORT = process.env.PORT || 3000;
 const app = express();
-
-
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to TenantShield!" });
 });
 
-app.listen(3000, () => {
-    console.log(`🚀 Server running on http://localhost:3000`);
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
 export default app;
